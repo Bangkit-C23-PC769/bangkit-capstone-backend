90015e63affa66f585db38de79ccba8298d706b7
Ahmad Rifai <ahmmd.riffai@gmail.com>
2023-06-07 22:35:09 +0700

    fix problem

--- routes/route-stations.js
+++ routes/route-stations.js
@@ -4,6 +4,5 @@ import StationController from '../controller/StationController.js'
 const router = express.Router()
 
 router.get('/stations', StationController.list)
-router.post('/stations', StationController.post)
 
 export default router
