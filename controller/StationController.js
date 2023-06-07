2b5b18f48eaea515fa983755597d9adfb45dcbbd
Ahmad Rifai <ahmmd.riffai@gmail.com>
2023-06-08 00:38:35 +0700

    make crud stations (create, detail, list, delete)

--- controller/StationController.js
+++ controller/StationController.js
@@ -12,6 +12,79 @@ class StationController {
             console.log(error)
         }
     }
+
+    static async create(req, res) {
+        try {
+            const { name, city, address, latitude, longitude } = req.body
+
+            let station = Stations.findOne({
+                where: {
+                    name
+                }
+            })
+
+            if (station !== null) return res.status(400).json({message: "Station already exist"})
+            
+            station = await Stations.create({
+                name, city, address, latitude, longitude
+            })
+
+            res.json({
+                success: true,
+                message: "Create data successfully",
+                data: station
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static async detail(req, res) {
+        try {
+            const {id} = req.params
+            const station = await Stations.findOne({
+                where: {
+                    id
+                }
+            })
+
+            res.json({
+                success: true,
+                message: "Success",
+                data: station
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static async delete(req, res) {
+        try {
+            const {id} = req.params
+
+            let station = Stations.findOne({
+                where: {
+                    id
+                }
+            })
+
+            if (station !== null) return res.status(400).json({message: "Id station not found"})
+
+            await Stations.destroy({
+                where: {
+                    id
+                }
+            })
+
+            res.json({
+                success: true,
+                message: "Delete Station Successfully"
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 export default StationController;
--- routes/route-stations.js
+++ routes/route-stations.js
@@ -4,5 +4,9 @@ import StationController from '../controller/StationController.js'
 const router = express.Router()
 
 router.get('/stations', StationController.list)
+router.post('/stations', StationController.create)
+router.get('/stations/:id', StationController.detail)
+router.delete('/stations/:id', StationController.delete)
+
 
 export default router
