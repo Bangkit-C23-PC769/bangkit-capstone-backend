44d29173f790c194a037301272d196485e2b4f8a
Ahmad Rifai <ahmmd.riffai@gmail.com>
2023-06-09 15:48:06 +0700

    add dockerize and configure socketpath

--- config/database.js
+++ config/database.js
@@ -2,14 +2,21 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const host = process.env.ç
 const user = process.env.DB_USER
 const pass = process.env.DB_PASS
 const dbName = process.env.DB_NAME
+const dbInstance = process.env.DB_INSTANCE
+const host = process.env.DB_HOST
 
 const db = new Sequelize(dbName, user, pass, {
-    host: host,
-    dialect: 'mysql'
+    host: process.env.ENV === 'prod' ? `/cloudsql/${dbInstance}`: host,
+    dialect: 'mysql',
+    dialectOptions: {
+        socketPath: process.env.ENV === 'prod' ? `/cloudsql/${dbInstance}`: null,
+    }
 });
+
+
+console.log(`Connecting to database : ${host},  ${user}, ${dbName}`)
 
 export default db;
